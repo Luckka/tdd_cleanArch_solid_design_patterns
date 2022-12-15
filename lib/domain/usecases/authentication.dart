3ee86e1c96d04d8de290d1300fd355740d578568
Lucka <lucka.dev.pj@dippingcode.com.br>
2022-12-15 09:24:51 -0300

    refactor authentication

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -2,8 +2,15 @@ import 'package:clean_arch/domain/entities/account_entity.dart';
 import 'package:flutter/material.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth({
-   @required String email,
-   @required String password
-  });
+  Future<AccountEntity> auth(AuthenticationParms params);
+}
+
+class AuthenticationParms {
+   final String email;
+   final String password;
+
+   AuthenticationParms({
+    @required this.email,
+    @required this.password
+   });
 }
